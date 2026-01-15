27f0b37dda53688471b3d37abe495031dfda0b0d
Scott Lu <scottlu6@gmail.com>
2026-01-15 08:25:30 -0500

    v2 - Added dynamic timestamped CSV filenames for daily snapshots

--- hackersNews.py
+++ hackersNews.py
@@ -1,13 +1,16 @@
 import requests
 import csv
+from datetime import datetime
 
 #get all top stories ID
 url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 reponse = requests.get(url)
 story_id = reponse.json()
 
+current_date = datetime.now().strftime("%Y-%m-%d")
+
 #write into csv ('with' auto closes file)
-with open("data/top_stories.csv", "w", newline="") as file:
+with open("data/top_stories_" + current_date + ".csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Rank", "Title", "Author", "Score"])
 
