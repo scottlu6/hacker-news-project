28b069a2291869b69d0b531edb2a559b7d34e361
Scott Lu <scottlu6@gmail.com>
2026-01-25 08:45:46 -0500

    v3.1- skip if yesterdays file wasnt saved

--- hackersNews.py
+++ hackersNews.py
@@ -15,15 +15,18 @@ yesterday_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 yesterday_data = {}
 
 #add yesterdays data in dict
-with open("data/snapshots/top_stories_" + yesterday_date +".csv", "r") as yesterday_file:
-    reader = csv.reader(yesterday_file)
-    next(reader) #skip first row
+try:
+    with open("data/snapshots/top_stories_" + yesterday_date +".csv", "r") as yesterday_file:
+        reader = csv.reader(yesterday_file)
+        next(reader) #skip first row
 
-    for row in reader:
-        storyID =int(row[1])
-        rank = int(row[0])
+        for row in reader:
+            storyID =int(row[1])
+            rank = int(row[0])
 
-        yesterday_data[storyID] = rank
+            yesterday_data[storyID] = rank
+except FileNotFoundError:
+    pass
     
 
 #add and update ranks
@@ -40,7 +43,7 @@ with open("data/snapshots/top_stories_" + current_date + ".csv", "w", newline=""
         for i in range(50):
             current_story_id = story_id[i]
 
-            #find first story info
+            #find story info
             item_url = f"https://hacker-news.firebaseio.com/v0/item/{current_story_id}.json"
             story_data = requests.get(item_url).json() 
             
